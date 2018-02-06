bc8f2af0627278c2b2dd260ece5f638cf8e7ac4c
ScorpioPK <andrei.andreiana@gmail.com>
2018-02-06 19:31:13 +0200

    Revert "Adding Google Sheets sample code"
    This reverts commit 0830a3c3e7f26d95e269dc51aa688962a93e3133.

--- app/src/main/java/com/example/scorpiopk/checklist/MainActivity.java
+++ app/src/main/java/com/example/scorpiopk/checklist/MainActivity.java
@@ -2,24 +2,16 @@ package com.example.scorpiopk.checklist;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
 import com.example.scorpiopk.checklist.lists.ListsPage;
 import com.example.scorpiopk.checklist.utils.FileHelper;
-import com.example.scorpiopk.checklist.utils.sheets.SheetsInitializer;
 
-import java.util.List;
-
-import pub.devrel.easypermissions.AfterPermissionGranted;
-import pub.devrel.easypermissions.EasyPermissions;
-
-public class MainActivity extends Activity implements EasyPermissions.PermissionCallbacks
+public class MainActivity extends Activity
 {
     private static MainActivity sInstance = null;
-    private SheetsInitializer mSheetsInitializer = null;
 
     public static MainActivity GetCurrentActivity() {
         return MainActivity.sInstance;
@@ -30,10 +22,7 @@ public class MainActivity extends Activity implements EasyPermissions.Permission
     {
         super.onCreate(savedInstanceState);
         sInstance = this;
-        //ShowMainScreen();
-
-        mSheetsInitializer = new SheetsInitializer(this);
-        mSheetsInitializer.getResultsFromApi();
+        ShowMainScreen();
     }
 
     public void DeleteList(String listName) {
@@ -56,8 +45,6 @@ public class MainActivity extends Activity implements EasyPermissions.Permission
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
         FileHelper.PermissionResult(requestCode, permissions, grantResults);
-        EasyPermissions.onRequestPermissionsResult(
-                requestCode, permissions, grantResults, this);
     }
 
     public void HideKeyboard() {
@@ -67,27 +54,4 @@ public class MainActivity extends Activity implements EasyPermissions.Permission
             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
     }
-
-    @AfterPermissionGranted(SheetsInitializer.REQUEST_PERMISSION_GET_ACCOUNTS)
-    public void chooseAccount() {
-        mSheetsInitializer.chooseAccount();
-    }
-
-    @Override
-    protected void onActivityResult(
-            int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        mSheetsInitializer.onActivityResult(requestCode, resultCode, data);
-    }
-
-    @Override
-    public void onPermissionsGranted(int requestCode, List<String> perms) {
-        mSheetsInitializer.onPermissionsGranted(requestCode, perms);
-    }
-
-    @Override
-    public void onPermissionsDenied(int requestCode, List<String> perms) {
-        mSheetsInitializer.onPermissionsDenied(requestCode, perms);
-    }
-
 }
